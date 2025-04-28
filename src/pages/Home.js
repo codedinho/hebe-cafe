7cc087b29b765f2387a9735d514281ba1f3d846f
Charlie <codedinho@gmail.com>
2025-04-28 14:53:46 +0100

    updated home pic + insta name

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,12 +4,16 @@ import Loader from '../components/Loader';
 import { useMinimumLoadingTime } from '../hooks/useMinimumLoadingTime';
 
 function Home() {
-
-
   return (
     <div className={styles.home}>
       <div
         className={styles.hero}
+        style={{
+          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), url(${process.env.PUBLIC_URL}/images/about-image-2.png)`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat'
+        }}
       >
         <img
           className={styles.heroLogo}
